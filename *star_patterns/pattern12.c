192f7877fa1f5fb358bbfcd17e9a40c86e982624
deepakguntoju <62128933+deepakguntoju@users.noreply.github.com>
2020-06-07 00:28:00 +0530

    Create pattern12.c

---
+++ *star_patterns/pattern12.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main ()
+{
+  int i,j,n = 7;
+  int count=0;
+  for (i = -n; i<=n; i++)
+    {
+      for (j = -n; j <=n; j++)
+	{
+	  if(abs(j)<=abs(i))
+	  printf ("* ");
+	  else
+	 printf("  ");
+	 count++;
+	}
+	count=0;
+	printf("\n");
+	}
+	return 0;
+}
+/*
+* * * * * * * * * * * * * * * 
+  * * * * * * * * * * * * *   
+    * * * * * * * * * * *     
+      * * * * * * * * *       
+        * * * * * * *         
+          * * * * *           
+            * * *             
+              *               
+            * * *             
+          * * * * *           
+        * * * * * * *         
+      * * * * * * * * *       
+    * * * * * * * * * * *     
+  * * * * * * * * * * * * *   
+* * * * * * * * * * * * * * * 
+
+
+
+*/
