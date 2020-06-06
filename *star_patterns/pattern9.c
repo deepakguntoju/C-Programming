c8fbc3d03109fd001f8b85f3e9f54d5ccdca64fc
deepakguntoju <62128933+deepakguntoju@users.noreply.github.com>
2020-06-07 00:04:26 +0530

    Create pattern9.c

---
+++ *star_patterns/pattern9.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main ()
+{
+  int i,j,n = 8;
+  int count=n;
+  for (i = 0; i<=n; i++)
+    {
+      for (j = 0; j <=n; j++)
+	{
+	  if(j>=count)
+	  printf ("* ");
+	  else
+	 printf(" ");
+	}
+	count--;
+	printf("\n");
+	}
+	return 0;
+}
+
+/*
+        * 
+       * * 
+      * * * 
+     * * * * 
+    * * * * * 
+   * * * * * * 
+  * * * * * * * 
+ * * * * * * * * 
+* * * * * * * * * 
+
+*/
